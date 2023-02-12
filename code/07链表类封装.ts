5d7ed119e691c6b1f7f356a5ee6c7f7b6662f2df
yunbaba520 <1650620783@qq.com>
2023-02-12 13:01:14 +0800

    链表方法

--- code/07链表类封装.ts
+++ code/07链表类封装.ts
@@ -69,6 +69,82 @@ class LinkedList<T> {
 
     return true;
   }
+  // 删除方法:
+  removeAt(position: number): T | null {
+    // 1.越界的判断
+    if (position < 0 || position >= this.size) return null;
+
+    // 2.判断是否是删除第一个节点
+    let current = this.head;
+    if (position === 0) {
+      this.head = current?.next ?? null;
+    } else {
+      let previous: Node<T> | null = null;
+      let index = 0;
+      while (index++ < position && current) {
+        previous = current;
+        current = current.next;
+      }
+
+      // 找到需要的节点
+      previous!.next = current?.next ?? null;
+    }
+
+    this.size--;
+
+    return current?.value ?? null;
+  }
+  // 获取方法:
+  get(position: number): T | null {
+    // 越界问题
+    if (position < 0 || position >= this.size) return null;
+
+    // 2.查找元素, 并且范围元素
+    let index = 0;
+    let current = this.head;
+    while (index++ < position && current) {
+      current = current.next;
+    }
+
+    // index === position
+    return current?.value ?? null;
+  }
+  // 更新某个值
+  update(position: number, value: T): boolean {
+    // 判断越界
+    if (position < 0 || position >= this.size) return false;
+    // 获取position对应node
+    let index: number = 0;
+    let current: Node<T> | null = this.head;
+    while (index < position && current) {
+      current = current?.next;
+      index++;
+    }
+    current!.value = value;
+    return true;
+  }
+  // 根据值获取索引
+  indexOf(value: T): number {
+    let index: number = 0;
+    let current: Node<T> | null = this.head;
+    while (index < this.size) {
+      if (value === current?.value) {
+        return index;
+      }
+      current = current!.next;
+      index++;
+    }
+    return -1;
+  }
+  // 根据value删除节点
+  remove(value: T): T | null {
+    const currentIndex = this.indexOf(value);
+    return this.removeAt(currentIndex);
+  }
+  // 判读单链表是否为空的方法
+  isEmpty() {
+    return this.size === 0;
+  }
 }
 
 const linkedList = new LinkedList<string>();
@@ -81,6 +157,22 @@ linkedList.traverse();
 console.log("```````````````````````插入");
 
 linkedList.insert("abc", 1);
+linkedList.traverse();
+console.log("```````````````````````删除");
+
+linkedList.removeAt(1);
+linkedList.traverse();
+console.log("```````````````````````get");
+console.log(linkedList.get(1));
+console.log("```````````````````````update");
+
+console.log(linkedList.update(1, "bcb"));
+
+linkedList.traverse();
+console.log("```````````````````````indexOf");
+console.log(linkedList.indexOf("ddd"));
+console.log("```````````````````````remove");
+linkedList.remove("ccc");
 linkedList.traverse();
 
 export {};
